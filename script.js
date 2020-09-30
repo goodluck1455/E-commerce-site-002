94e1e52550d06178e6820715d78ee6d653077fce
goodluck1455 <goodluckstephen237@gmail.com>
2020-09-30 16:12:15 +0100

    another commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let permIdentity = document.querySelector(".perm-identity-2");
+let loginHover = document.querySelector(".loginHover");
+
+let helpoutline = document.querySelector(".help-outline-2");
+let loginHoverB = document.querySelector(".loginHoverB");
+
+
+// permIdentity.addEventListener("click", ()=>{
+//     if(body !== "click"){
+//         loginHover.style.display = "block";
+//     } else{
+        
+//         loginHover.style.display = "none"; 
+//     }
+    
+    // loginHover.style.display = "block";
+
+
+    // if(helpoutline === true){
+    //     loginHover.style.display = "none";
+    // }else{
+    // loginHover.classList.toggle("loginHoverA");
+    // return;}
+})
+
+// helpoutline.addEventListener("onmouseover", ()=>{
+//     loginHoverB.style.display = "block";
+//     // return;
+// })
+// function mouse(){
+//     loginHover.classList.toggle("loginHoverA");
+// }
